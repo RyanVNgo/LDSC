2b08d3ad2e2cba535687486468f0c384890b2121
RyanVNgo <vanryan711@gmail.com>
2024-07-17 22:15:18 -0700

    First implementation of queue feature
    Tests still need to be written for all queue functions

--- include/LDSC_queue.h
+++ include/LDSC_queue.h
@@ -3,4 +3,11 @@
 
 typedef struct LDSC_queue LDSC_queue;
 
+LDSC_queue* LDSC_queue_init();
+int LDSC_queue_length(LDSC_queue*);
+int LDSC_queue_isEmpty(LDSC_queue*);
+void LDSC_queue_enqueue(LDSC_queue*, void*);
+void* LDSC_queue_dequeue(LDSC_queue*);
+void* LDSC_queue_peek(LDSC_queue*);
+
 #endif
