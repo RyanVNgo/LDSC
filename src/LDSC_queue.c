8cfadefb139f46adcfc8a162c241b384f60fbcf5
RyanVNgo <vanryan711@gmail.com>
2024-07-19 14:16:34 -0700

    Removed comments from src files
    Removed many of the comments that explained each step in a method for every stuctures source file. Methods now only have a single comment before the definition explaining the function of the method.
    
    Additionally adjusted the implementation of some methods for optimization.

--- src/LDSC_queue.c
+++ src/LDSC_queue.c
@@ -1,11 +1,13 @@
 #include <LDSC_queue.h>
 #include <stdlib.h>
 
+// node definition
 typedef struct LDSC_node {
   void* dataPtr;
   struct LDSC_node* next;
 } LDSC_node;
 
+// create and return a new node
 static LDSC_node* LDSC_node_init(void* dataPtrIn) {
   LDSC_node* newNode = (LDSC_node*)malloc(sizeof(LDSC_node));
   newNode->dataPtr = dataPtrIn;
@@ -13,12 +15,14 @@ static LDSC_node* LDSC_node_init(void* dataPtrIn) {
   return newNode;
 }
 
+// queue definition
 struct LDSC_queue {
   int length;
   LDSC_node* head;
   LDSC_node* tail;
 };
 
+// create and return a new queue
 LDSC_queue* LDSC_queue_init() {
   LDSC_queue* newQueue = (LDSC_queue*)malloc(sizeof(LDSC_queue));
   newQueue->length = 0;
@@ -27,43 +31,45 @@ LDSC_queue* LDSC_queue_init() {
   return newQueue;
 }
 
+// return length of the queue
 int LDSC_queue_length(LDSC_queue* queueIn) {
   if (!queueIn) return -1;
   return queueIn->length;
 }
 
-int LDSC_queue_isEmpty(LDSC_queue* queueIn) {
-  if (!queueIn) return -1;
-  if (!queueIn->length) return 1;
-  return 0;
-}
-
+// add item to the back of the queue
 void LDSC_queue_enqueue(LDSC_queue* queueIn, void* dataPtrIn) {
   if (!queueIn || !dataPtrIn) return;
   LDSC_node* newNode = LDSC_node_init(dataPtrIn);
+
   if (!queueIn->head) {
     queueIn->head = newNode;
-    queueIn->tail = newNode;
   } else {
     queueIn->tail->next = newNode;
-    queueIn->tail = newNode;
   }
+  
+  queueIn->tail = newNode;
   queueIn->length++;
   return;
 }
 
+// remove and return item at the front of the queue
 void* LDSC_queue_dequeue(LDSC_queue* queueIn) {
   if (!queueIn || !queueIn->length) return NULL;
+
   LDSC_node* oldHead = queueIn->head;
   void* dataPtrOut = oldHead->dataPtr;
   queueIn->head = oldHead->next;
+
   if (!queueIn->head)
     queueIn->tail = NULL;
+
   queueIn->length--;
   free(oldHead);
   return dataPtrOut;
 }
 
+// return item at the front of the queue
 void* LDSC_queue_peek(LDSC_queue* queueIn) {
   if (!queueIn || !queueIn->head) return NULL;
   return queueIn->head->dataPtr;
--- src/LDSC_stack.c
+++ src/LDSC_stack.c
@@ -31,35 +31,25 @@ LDSC_stack* LDSC_stack_init() {
 
 // return length of the stack
 int LDSC_stack_length(LDSC_stack* stackIn) {
-  // condition for invalid parameters
   if (!stackIn) return -1;
-
   return stackIn->length;
 }
 
 // check if the stack is empty
 int LDSC_stack_isEmpty(LDSC_stack* stackIn) {
-  // condition for invalid parameters
   if(!stackIn) return -1;
-
-  // return 1 for empty list, 0 otherwise
   if (!stackIn->length) return 1;
   return 0;
 }
 
 // push new data to the stack
 void LDSC_stack_push(LDSC_stack* stackIn, void* dataPtrIn) {
-  // conditions for invalid parameters
   if (!stackIn || !dataPtrIn) return;
 
-  // create new node and assign new node's next to current top
   LDSC_node* newNode = LDSC_node_init(dataPtrIn);
   newNode->next = stackIn->top;
-
-  // set top of stack to new node
   stackIn->top = newNode;
-  
-  // increment length of stack
+
   stackIn->length++;
   return;
 }
@@ -72,19 +62,12 @@ void* LDSC_stack_peek(LDSC_stack* stackIn) {
 
 // remove and return data on top of the stack
 void* LDSC_stack_pop(LDSC_stack* stackIn) {
-  // conditions for invalid parameters
   if (!stackIn || !stackIn->top) return NULL;
   
-  // retrieve data ptr of current top
   void* dataOutPtr = stackIn->top->dataPtr;
-
-  // remember new top
   LDSC_node* newTop = stackIn->top->next;
-
-  // free current top
+  
   free(stackIn->top);
-
-  // assign new top
   stackIn->top = newTop;
 
   return dataOutPtr;
